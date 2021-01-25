e91fad7e6666da8766ccffc7f60ce17c32c40e4e
jenniferlang1921 <jenniferlang1921@gmail.com>
2021-01-24 19:18:30 -0600

    first commit

---
+++ src/App.vue
@@ -0,0 +1,96 @@
+<template>
+  <div id="app">
+     
+    <img id="MountainImg" alt="man climbing mountain" src="./assets/mountain.jpg">
+   
+    <Header />
+    <router-view/>
+    <footer>
+      <div id="myName">
+        <a href="https://jenniferlang1921.com">Jennifer Lang Web Design</a>
+      </div>
+      <div id="Attribution">
+        <p>Thank you to the following:</p>
+        <a href="https://www.vecteezy.com/free-vector/corporate-goals">Corporate Goals Vectors by Vecteezy</a><br>
+        <a href="https://youtu.be/Wy9q22isx3U">Vue JS Crash Course Youtube Traversy Media</a>
+        </div>
+    </footer>
+  </div>
+</template>
+
+<script>
+import Header from './components/layout/Header';
+export default {
+  name:"app",
+  components: {
+    Header
+  }
+}
+</script>
+
+
+<style>
+  * {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+  }
+  body {
+    font-family: Arial, Helvetica, sans-serif;
+    line-height: 1.4;
+    font-size: 150%;
+  }
+  .btn {
+    display: inline-block;
+    border: none;
+    background: #286E8F !important;
+    color: #fff;
+    padding: 7px 20px;
+    font-size: 150%;
+    cursor: pointer;
+  }
+  .btn:hover {
+    background: #C9E9F4 !important;
+    color: #000;
+  
+  }
+
+  #MountainImg {
+    width: 100%;
+  }
+
+  footer {
+    background: #2A0E3F;
+    color: #fff;
+    padding: 100px;
+    font-size: 80%;
+    margin-top: 5%;
+    
+  }
+  footer a {
+    color: #fff;
+    text-decoration: none;
+  }
+
+  footer a:hover {
+    color: #C9E9F4;
+    text-decoration: underline;
+  }
+
+
+
+  #myName{
+    margin-bottom: 1%;
+    width: 50%;
+    font-size: 150%;
+    float: left;
+  }
+
+  
+
+  #Attribution {
+    color: #fff;
+    width: 40%;
+    float: right;
+  }
+</style>
---
+++ src/components/layout/Header.vue
@@ -0,0 +1,37 @@
+<template>
+  <header class="header">
+    <h1>TodoList</h1>
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "Header"
+}
+</script>
+
+<style scoped>
+  .header {
+    background: #2A0E3F;
+    color: #fff;
+    text-align: center;
+    padding: 10px;
+    margin-top: -10px;
+  }
+  .header a {
+    color: #fff;
+    padding-right: 5px;
+    text-decoration: none;
+  }
+
+   .header a:hover {
+    color: #C9E9F4;
+    text-decoration: underline;
+  }
+
+    
+</style>
